801b9a6bd9cc40923e968f989d24f4063d785001
narayan <anedevkota@yahoo.com>
2015-12-02 17:15:32 -0600

    creating home and about pages with hyperlink

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,8 @@ Ruby::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
-
+  root to: 'pages#home'
+  get 'about' => "pages#about"
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
